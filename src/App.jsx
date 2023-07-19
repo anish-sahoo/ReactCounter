388e1cc2a6c421517e1cc77730f1bf42d601ab62
Palash Bera <palashbera1234@gmail.com>
2023-07-19 19:46:37 +0530

    Adding counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import "./stylesheets/App.css";
 
 const App = () => {
+  const counter = 0;
+
   return (
-    <h1>Hello World..!!</h1>
+    <div className="container">
+      <div className="main-content">
+        <h2 className="header">My Counter</h2>
+
+        <div className="display-box">{counter}</div>
+
+        <div className="button-group">
+          <button className="button danger" type="button">Down</button>
+          <button className="button warning" type="button">Reset</button>
+          <button className="button primary" type="button">Up</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
