515c7410b0d47be4cb57f30ed4173ec21917de08
Anish Sahoo <anishsahoo2005@gmail.com>
2023-07-19 15:59:12 -0500

    added many things

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,54 @@
 import React, { useState } from 'react';
 import "./style/App.css";
 import toast, { Toaster } from 'react-hot-toast';
+import Confetti from 'react-confetti';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [negCount, setNeg] = useState(0);
+  const [confetti, setConfetti] = useState(false);
 
+  const a = ['Why do you disappoint yourself?', 'Why do you disappoint us?', 'Try clicking reset'];
+  
   const handleDown = () => {
-    if(counter == 0){
+    if(counter === 0){
+      if(negCount === 2){
+        setNeg(0);
+        handleConfetti();
+      }
       setNeg(negCount + 1);
-      toast('Why do you disappoint yourself?')
+      toast.error(a[negCount%3])
     }
     else {
       setCounter(counter - 1);
     }
   };
 
+  const handleReset = () => {
+    toast.success('Reset Successful')
+    setCounter(0);
+    setNeg(0);
+  };
+
+  const handleUp = () => {
+    if((counter+1)%10 === 0){
+      toast.success('Great job! You reached '+(counter+1));
+    }
+    if((counter+1)%5 === 0){
+      handleConfetti();
+    }
+    setCounter(counter + 1);
+  }
+
+  const handleConfetti = () => {
+    setConfetti(true);
+    setTimeout(() => setConfetti(false), 5000);
+  };
+
   return (
     <div className="container">
+      <Toaster/>
+      <div className='confetti'>{confetti && <Confetti width={window.innerWidth} height={window.innerHeight} wind={0.02}/>}</div>
       <div className="main-content">
         <h2 className="header">My Counter</h2>
 
@@ -25,8 +56,8 @@ const App = () => {
 
         <div className="button-group">
           <button className="button danger" type="button" onClick={() => handleDown()}>Down</button>
-          <button className="button warning" type="button" onClick={() => setCounter(0)}>Reset</button>
-          <button className="button primary" type="button" onClick={() => setCounter(counter + 1)}>Up</button>
+          <button className="button warning" type="button" onClick={() => handleReset()}>Reset</button>
+          <button className="button primary" type="button" onClick={() => handleUp()}>Up</button>
         </div>
       </div>
     </div>
